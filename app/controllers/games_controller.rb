a06bc8bf8cb618b8b44787872bf6d7b05ac2c923
TakeumiYamamura <yama1223xxx@gmail.com>
2016-07-01 18:06:18 +0900

    result

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,9 +11,9 @@ class GamesController < ApplicationController
     #   redirect_to action: :index, notice: '仮想マシンの起動に失敗しました'
     # end
     begin
-      system(vpgu_start_command)
-      binding.pry
-      redirect_to action: :index, success: '仮想マシンの起動に成功しました！！'
+      result = `#{vpgu_start_command}`
+      # system(vpgu_start_command)
+      redirect_to action: :index, success: "仮想マシンの起動に成功しました！！#{result}"
     rescue
       redirect_to action: :index, danger: '仮想マシンの起動に失敗しました'
     end
