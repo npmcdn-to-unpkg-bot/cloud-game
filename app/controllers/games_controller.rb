2eb662dac62c62b5defc4e10731ff229eca7abcd
TakeumiYamamura <yama1223xxx@gmail.com>
2016-07-01 18:48:17 +0900

    change url

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,7 +13,7 @@ class GamesController < ApplicationController
     begin
       result = `#{vpgu_start_command}`
       # system(vpgu_start_command)
-      redirect_to result, success: "仮想マシンの起動に成功しました！！#{result}"
+      redirect_to "http://157.82.3.170:3000/done.html", success: "仮想マシンの起動に成功しました！！#{result}"
     rescue
       redirect_to action: :index, danger: '仮想マシンの起動に失敗しました'
     end
