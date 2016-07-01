6ff57351dc45f13734442cbe82fc497f00358191
TakeumiYamamura <yama1223xxx@gmail.com>
2016-07-01 15:27:15 +0900

    Add execute command

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,14 @@
 class GamesController < ApplicationController
-	def index
-		
-	end
+
+  def index
+  end
+
+  def show
+    vpgu_start_command = "vgpu start #{current_user.id} #{params[:id]}"
+    if system.(vgpu_start_command)
+      redirect_to action: :index, notice: '仮想マシンの起動に成功しました！！'
+    else
+      redirect_to action: :index, notice: '仮想マシンの起動に失敗しました'
+    end
+  end
 end
